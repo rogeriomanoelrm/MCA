5994497da0dca34d4d8dddd602eb7ea84a7f0d8d
rogeriomanoelrm <77088268+rogeriomanoelrm@users.noreply.github.com>
2024-02-07 19:35:25 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<link rel="stylesheet" href="mca12.css">
+<link rel="stylesheet" href="mca15.css">
 </head>
 <body id="paginainicial">
 <body id="corpo">
@@ -14,7 +14,7 @@
 
         <nav>
 
-        <img class="mcalogo" src="./img/logo.gif" alt="">
+        <img class="mcalogo" src="./img/logomemaform.gif" alt="">
 
 
         <img id="bt-fechar" src="./img/bt-fechar.png" alt="">
@@ -37,12 +37,11 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
 
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="carro" src="./img/jesus.gif" alt="Primeiro Slide">
+      <img class="carro" src="./img/semlimites.gif" alt="Primeiro Slide">
     </div>
     <div class="carousel-item">
       <img class="carro" src="./img/encontro.jpg.gif" alt="Segundo Slide">
@@ -50,9 +49,7 @@
     <div class="carousel-item">
       <img class="carro" src="./img/escola.jpg.gif" alt="Terceiro Slide">
     </div>
-    <div class="carousel-item">
-      <img class="carro" src="./img/semlimites.gif" alt="Quarto Slide">
-    </div>
+    
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -65,10 +62,9 @@
 </div> 
 
 
-
 <script src="./JS/jquery-3.7.1.min.js"></script>
 <script src="./JS/menu-hamburguer.js"></sCRipt>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
