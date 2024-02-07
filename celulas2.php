902f0372b367589b1c391b6404378355e4d1d7d1
rogeriomanoelrm <77088268+rogeriomanoelrm@users.noreply.github.com>
2024-02-07 19:34:20 -0300

    Update celulas2.php

--- celulas2.php
+++ celulas2.php
@@ -5,79 +5,82 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="mca8.css">
+    <link rel="stylesheet" href="mca.css">
     <title>MCA</title>
-    <!-- Mantenha apenas uma referência ao jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
 </head>
 
 <body>
     <header id="ham">
+
         <nav>
+
             <img class="mcalogo" src="./img/logomca.gif" alt="">
+
+
             <img id="bt-fechar" src="./img/bt-fechar.png" alt="">
             <img id="bt-hamburguer" src="./img/bt-hamburguer.png" alt="">
+
             <ul class="menu-principal">
                 <lu>Home</lu>
                 <lu>Sobre</lu>
                 <lu><a href="">Celulas</a></lu>
                 <lu><a href="form.php">Cadastro</a></lu>
+
             </ul>
         </nav>
     </header>
 
-    <div>
-        <select id="localSelect" name="opcoes">
-            <option value="">Escolha um bairro</option>
-            <option value="silveira">Jardim Silveira</option>
-            <option value="imperial">Parque Imperial</option>
-            <option value="reginalice">Reginalice</option>
-        </select>
+
+<main>
+
+
+    <div id="silveira">
+        <img src="./img/silveira.jpg" alt="">
+        <p>Líder: Rogério/ Aux.: Ester - 20:00 – Av. Presidente Washington Luiz, 331, Viela casa 10, Jardim Silveira – Barueri/SP (Terça- feira)</p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.131736139902!2d-46.89725742731539!3d-23.52776381353337!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf015a04fbe081%3A0xae4872954aa54124!2sAv.%20Presidente%20Washington%20Luis%2C%20331%20-%20casa%2010%20-%20Jardim%20Silveira%2C%20Barueri%20-%20SP%2C%2006434-000!5e0!3m2!1sen!2sbr!4v1707164506393!5m2!1sen!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><p style="color:aliceblue;">Contate o líder</p>
+            <a href="https://wa.me/5511934663027?text=Hello!%20I'm%20interested%20in%20your%20services." target="_blank">
+    <img class="whats" src="./img/whatsapp.png" alt="WhatsApp" style="height:50px; width:50px;">
+</a> 
+
+
+
     </div>
 
-    <!-- Corrigido para conteudoPlaceholder -->
-    <div id="conteudoPlaceholder"></div>
 
-    <script>
-        document.getElementById('localSelect').addEventListener('change', async function() {
-            var valor = this.value;
-            var divConteudo = document.getElementById('conteudoPlaceholder');
+    <div id="imperial">
 
-            // Limpar conteúdo anterior
-            divConteudo.innerHTML = '';
+        <img src="./img/imperial.jpg" alt="">
+        <p>Líder: Amanda/ Aux.: Caio – 20:00 - Rua Adoniran Barbosa, 323 - Pq Imperial - Barueri - SP (Sexta-Feira)</p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.2875000267945!2d-46.8104626!3d-23.4861519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cefde6747fd481%3A0x13cd9fa358efe902!2sR.%20Adoniran%20Barbosa%2C%20323%20-%20Parque%20Imperial%2C%20Barueri%20-%20SP%2C%2002675-031!5e0!3m2!1sen!2sbr!4v1707168920520!5m2!1sen!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
-            // Verificar se um valor foi selecionado
-            if (valor) {
-                // URL da página da qual você quer extrair o conteúdo
-                var urlDaPagina = 'celulas2.php?bairro=' + valor; // Ajuste conforme necessário
+    </div>
 
-                try {
-                    // Fazendo a requisição para a página
-                    const resposta = await fetch(urlDaPagina);
-                    const texto = await resposta.text();
-                    
-                    // Convertendo o texto para um elemento HTML
-                    const html = new DOMParser().parseFromString(texto, 'text/html');
-                    
-                    // Selecionando o conteúdo específico que você quer da página
-                    const conteudoEspecifico = html.querySelector('.' + valor); // Ex: .silveira, .imperial, .reginalice
-                    
-                    // Verificando se o conteúdo foi encontrado
-                    if(conteudoEspecifico) {
-                        // Adicionando o conteúdo ao divConteudo
-                        divConteudo.appendChild(conteudoEspecifico);
-                    } else {
-                        divConteudo.innerHTML = 'Conteúdo não encontrado.';
-                    }
-                } catch (erro) {
-                    console.error('Houve um erro ao buscar a página:', erro);
-                    divConteudo.innerHTML = 'Não foi possível carregar o conteúdo.';
-                }
-            }
-        });
-    </script>
+    <div id="reginalice">
 
-    <script src="./JS/menu-hamburguer.js"></script>
+        <img src="./img/reginalice.jpg" alt="">
+        <p>Líder: Camile/ Aux.: Tereza – 20:00 – Rua mar vermelho,533 - Jd Regina Alice - Barueri - SP (Quarta-Feira)</p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.9428758448757!2d-46.883758925424516!3d-23.498566959286805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf03bf9e996375%3A0x8c2df3fe9b235550!2sR.%20Mar%20Vermelho%2C%20533%20-%20Jardim%20Regina%20Alice%2C%20Barueri%20-%20SP%2C%2006412-140!5e0!3m2!1sen!2sbr!4v1707169043547!5m2!1sen!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+    </div>
+
+    <div id="tupanci">
+
+        <img src="./img/" alt="">
+        <p> Lider: Brenda, Aux: Paloma 20:00 hrs Rua Urano 151 - Jardim Tupanci - Barueri - SP</p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.049334388117!2d-46.87489912542476!3d-23.49473245914632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf03b7bce9e60d%3A0x23a6f126b4cb8e91!2sR.%20Urano%2C%20151%20-%20Jardim%20Tupanci%2C%20Barueri%20-%20SP%2C%2006414-020!5e0!3m2!1sen!2sbr!4v1707169123991!5m2!1sen!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+    </div>
+
+
+    </div>
+
+    </main>
+
+
+
 </body>
 
 </html>
